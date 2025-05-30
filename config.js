f79cd8978032fd4cd2121895bf611b806ae856d2
tech878 <bot442315@gmail.com>
2025-05-30 14:45:11 -0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "INCONNU~XD~",
+SESSION_ID: process.env.SESSION_ID || "INCONNU~XD~DY5xDSpa#6OUOXZJcdmDlDen9zjJNDTkPYhyP7IN2YCE964O1ksk",
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
